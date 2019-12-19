65c0a65cd9dd0432c0f370f8af59e5675859c2b4
iamnotahumanbecauseiamabot <42443321+iamnotahumanbecauseiamabot@users.noreply.github.com>
2019-12-20 00:27:52 +0530

    Update ciou.py

--- ciou.py
+++ ciou.py
@@ -7,11 +7,12 @@ def compute_ciou(target,  output):
     but can work for a single bounding box too
     all the boundary cases such as bounding boxes of size 0 are handled.
     ''' 
+    target = (target)*(target != 0)
+    output = (output)*(target != 0)
+
     x1g, y1g, x2g, y2g = tf.split(value=target, num_or_size_splits=4, axis=1)
     x1, y1, x2, y2 = tf.split(value=output, num_or_size_splits=4, axis=1)
-
-    x2 = tf.maximum(x1, x2)
-    y2 = tf.maximum(y1, y2)
+    
     w_pred = x2 - x1
     h_pred = y2 - y1
     w_gt = x2g - x1g
@@ -22,39 +23,48 @@ def compute_ciou(target,  output):
     x_center_g = (x1g + x2g) / 2
     y_center_g = (y1g + y2g) / 2
 
-    xkis1 = tf.maximum(x1, x1g)
-    ykis1 = tf.maximum(y1, y1g)
-    xkis2 = tf.minimum(x2, x2g)
-    ykis2 = tf.minimum(y2, y2g)
-
     xc1 = tf.minimum(x1, x1g)
     yc1 = tf.minimum(y1, y1g)
     xc2 = tf.maximum(x2, x2g)
     yc2 = tf.maximum(y2, y2g)
+    
+    ###iou term###
+    xA = tf.maximum(x1g, x1)
+    yA = tf.maximum(y1g, y1)
+    xB = tf.minimum(x2g, x2)
+    yB = tf.minimum(y2g, y2)
 
-    min_ymax = tf.minimum(y2, y2g)
-    max_ymin = tf.maximum(y1, y1g)
-    intersect_heights = tf.maximum(0.0, min_ymax - max_ymin)
-    min_xmax = tf.minimum(x2, x2g)
-    max_xmin = tf.maximum(x1, x1g)
-    intersect_widths = tf.maximum(0.0, min_xmax - max_xmin)
+    interArea = tf.maximum(0.0, (xB - xA + 1)) * tf.maximum(0.0, yB - yA + 1)
 
-    intsctk = intersect_heights * intersect_widths
-    unionk = (x2 - x1) * (y2 - y1) + (x2g - x1g) * (y2g - y1g) - intsctk + 1e-7
-    iouk = intsctk / unionk
+    boxAArea = (x2g - x1g +1) * (y2g - y1g +1)
+    boxBArea = (x2 - x1 +1) * (y2 - y1 +1)
 
+    iouk = interArea / (boxAArea + boxBArea - interArea + 1e-10)
+    ###
+    
+    ###distance term###
     c = ((xc2 - xc1) ** 2) + ((yc2 - yc1) ** 2) +1e-7
     d = ((x_center - x_center_g) ** 2) + ((y_center - y_center_g) ** 2)
     u = d / c
+    ###
 
-    arctan = tf.atan(w_gt/h_gt)-tf.atan(w_pred/h_pred)
-    v = (4 / (math.pi ** 2)) * tf.pow((tf.atan(w_gt/h_gt)-tf.atan(w_pred/h_pred)),2)
+    ###aspect-ratio term###
+    arctan = tf.atan(w_gt/(h_gt + 1e-10))-tf.atan(w_pred/(h_pred + 1e-10))
+    v = (4 / (math.pi ** 2)) * tf.pow((tf.atan(w_gt/(h_gt + 1e-10))-tf.atan(w_pred/(h_pred + 1e-10))),2)
     S = 1 - iouk
-    alpha = v / (S + v)
+    alpha = v / (S + v + 1e-10)
     w_temp = 2 * w_pred
-    
     ar = (8 / (math.pi ** 2)) * arctan * ((w_pred - w_temp) * h_pred)
+    ###
+    
+    ###calculate diou###
+    #diouk = iouk - u
+    #diouk = (1 - diouk)
+    ###
+    
+    ###calculate ciou###
     ciouk = iouk - (u + alpha * ar)
     ciouk = (1 - ciouk)
-    ciouk = tf.where(tf.is_nan(ciouk), tf.zeros_like(ciouk), ciouk)
+    ###
+    
     return ciouk
